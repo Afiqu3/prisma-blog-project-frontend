56f1b8511d0ebd128edd3661d08af2bda00e8910
Afiqu3 <gahossainj@gmail.com>
2026-07-20 21:32:57 +0600

    feat: add role-based redirection after login and include jsonwebtoken dependency

--- app/(auth)/_actions/authActions.ts
+++ app/(auth)/_actions/authActions.ts
@@ -1,7 +1,8 @@
 "use server";
 
 import { cookies } from "next/headers";
-// import { redirect } from "next/navigation";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { redirect } from "next/navigation";
 
 type LoginState = {
   success: boolean;
@@ -50,6 +51,16 @@ export const loginAction = async (
       maxAge: 60 * 60 * 24 * 7,
       sameSite: "lax",
     });
+
+    const decodedToken = jwt.decode(result.data.accessToken) as JwtPayload;
+
+    if (decodedToken.role === "USER") {
+      redirect("/dashboard");
+    } else if (decodedToken.role === "ADMIN") {
+      redirect("/admin-dashboard");
+    } else if (decodedToken.role === "AUTHOR") {
+      redirect("/author-dashboard");
+    }
 
     // redirect("/dashboard");
   }
