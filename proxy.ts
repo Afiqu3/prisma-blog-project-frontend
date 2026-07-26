9895175a03de5ee69caaf772b69b6cf8459c9a14
Afiqu3 <gahossainj@gmail.com>
2026-07-26 09:58:38 +0600

    feat: implement subscription status check for premium access and redirect to payment if inactive

--- proxy.ts
+++ proxy.ts
@@ -4,6 +4,7 @@ import { NextRequest } from "next/server";
 import { jwtUtils } from "./utils/jwt";
 import { getNewAccessToken } from "./service/refreshToken";
 import { JwtPayload } from "jsonwebtoken";
+import { getSubscriptionStatus } from "./app/(public)/_actions/getSubscriptionStatus";
 
 const AUTH_ROUTES = ["/login", "/register"];
 // const PUBLIC_ROUTES = ["/", "/news", "/login", "/register"]
@@ -91,6 +92,36 @@ export async function proxy(request: NextRequest) {
   ) {
     return NextResponse.redirect(new URL("/not-found", request.url));
   }
+
+  // const subscriptionStatus = await getSubscriptionStatus();
+
+  // const isActive = Boolean(
+  //     subscriptionStatus?.success && subscriptionStatus.data?.isSubscribed,
+  // );
+
+  if (pathname === "/premium") {
+    const subscriptionStatus = await getSubscriptionStatus();
+
+    const isActive = Boolean(
+      subscriptionStatus?.success && subscriptionStatus.data?.isSubscribed,
+    );
+
+    if (!isActive) {
+      return NextResponse.redirect(new URL("/payment", request.url));
+    }
+  }
+
+  // if(pathname === "/payment"){
+  //     // const subscriptionStatus = await getSubscriptionStatus();
+
+  //     // const isActive = Boolean(
+  //     //     subscriptionStatus?.success && subscriptionStatus.data?.isSubscribed,
+  //     // );
+
+  //     if (isActive) {
+  //         return NextResponse.redirect(new URL("/premium", request.url))
+  //     }
+  // }
 
   return NextResponse.next();
 }
